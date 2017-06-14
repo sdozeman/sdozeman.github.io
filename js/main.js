3c020de4e130ff6a8391cac20cf3c693c59f58f6
Sean Dozeman <sdozeman@sitecrafting.com>
2017-06-14 10:13:06 -0700

    added mobile navigation functionality

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
+// @codekit-prepend  "navigation.js"
+
 jQuery(document).ready(function($){
   $('.site--hero-background').parallaxify({
     positionProperty: 'transform',
     motionType: 'natural',
     responsive: true
   });
+
+  $('.site--navigation').mobileNavigation();
 
   // Sticky Header animation
   $(window).on('scroll touchmove', function(){
---
+++ js/navigation.js
@@ -0,0 +1,50 @@
+(function($) {
+  $.fn.mobileNavigation = function(options) {
+  	options = $.extend({}, {
+  		navSelector: '.site--navigation',
+  		menuButtonSelector: '.site--navigation-btn',
+  		menuOpenClass: 'menu-open',
+  		menuButtonActiveClass: 'active',
+  	}, options);
+
+  	var menuOpen = false,
+  		$nav = $(options.navSelector),
+  		$menuButton = $(options.menuButtonSelector);
+
+
+  	var closeNav = function() {
+      $nav.slideUp();
+  		$nav.removeClass(options.menuOpenClass);
+  		$menuButton.removeClass(options.menuButtonActiveClass);
+
+  		menuOpen = false;
+  	};
+
+  	var menuBtnFn = function() {
+
+  		$menuButton.bind('touchstart, click', function(event) {
+  			event.stopPropagation();
+  			event.preventDefault();
+
+  			if (menuOpen) {
+  				closeNav();
+  			}
+  			else{
+          $nav.slideDown();
+  				$nav.addClass( options.menuOpenClass );
+  				$(this).addClass( options.menuButtonActiveClass );
+  				menuOpen = true;
+  			}
+  		});
+  	};
+
+  	menuBtnFn();
+
+  	$(window).resize(function(){
+  		if( !$menuButton.is(':visible') ) {
+  			$nav.removeAttr( 'style' );
+  		}
+  	});
+  	return this;
+  };
+})(jQuery);
