b8c9e79c974a0e2a77fe4e009181affbd4492038
Sean Dozeman <sdozeman@sitecrafting.com>
2017-06-14 10:36:59 -0700

    Adding some test functionality for project section to fade/slide slightly.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 // @codekit-prepend  "navigation.js"
 
+//Functions Up Here
+function isScrolledIntoView(elem) {
+  var docViewTop = $(window).scrollTop();
+  var docViewBottom = docViewTop + $(window).height();
+
+  var elemTop = $(elem).offset().top;
+  var elemBottom = elemTop + $(elem).height();
+
+  return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+
+
+// On Ready Here
 jQuery(document).ready(function($){
   $('.site--hero-background').parallaxify({
     positionProperty: 'transform',
@@ -12,5 +26,10 @@ jQuery(document).ready(function($){
   // Sticky Header animation
   $(window).on('scroll touchmove', function(){
     $('.site-header').toggleClass('scrolled', $(document).scrollTop() > 100);
+    $('.project').each(function() {
+      if (isScrolledIntoView($(this))) {
+        $(this).addClass('in-viewport');
+      }
+    });
   }).scroll();
 });
